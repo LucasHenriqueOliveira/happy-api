a2d3797cea20f75189ce31b664f8f242f1c06482
Lucas <lucashen@gmail.com>
2020-10-20 18:38:07 -0300

    create route and add dependencies

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import { join } from 'path';
+import 'express-async-errors';
+
+import routes from './routes';
+import errorHandler from './errors/handler';;
+import './database/connection';
+
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use(errorHandler);
+
+app.listen(3333);
