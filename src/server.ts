a3c44623c8380f42a0cc87f4124cf08ae8b040a2
Lucas <lucashen@gmail.com>
2020-10-21 14:07:29 -0300

    add endpoint uploads

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,15 @@ import { join } from 'path';
 import 'express-async-errors';
 
 import routes from './routes';
-import errorHandler from './errors/handler';;
+import errorHandler from './errors/handler';
 import './database/connection';
 
-const app = express()
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
 
 app.listen(3333);
